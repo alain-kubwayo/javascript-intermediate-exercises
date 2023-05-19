44b2747cfc43606807693b3dfa4b5d1cd6ccdd51
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-19 10:15:06 +0200

    feat: refactor reverse string

--- stringReversal.js
+++ stringReversal.js
@@ -3,13 +3,8 @@ Given a string, reverse it. E.g. reverse('apple') === 'elppa'
 */
 
 const reverseString = str => {
-    const strArray = str.split('');
-    let strReversed = '';
-    for(let i=strArray.length-1; i>=0; i--){
-        strReversed += strArray[i];
-    }
-    return strReversed;
+    return str.split('').reverse().join('');
 }
 
-const result = reverseString('greetings!');
+const result = reverseString('apple');
 console.log(result);
